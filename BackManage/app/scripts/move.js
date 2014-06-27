af70c2ca52802e1aabd8fbfe41ed8854b31c94a7
Martin0809 <maning0809@gmail.com>
2014-06-27 21:24:11 +0800

    增加返回按钮

--- BackManage/app/scripts/move.js
+++ BackManage/app/scripts/move.js
@@ -3,3 +3,9 @@ function move() {
 	document.getElementById("second").style.left = "0";
 	document.getElementById("second").style.display = "block";
 }
+
+function back() {
+	document.getElementById("first").style.left = "0";
+	document.getElementById("second").style.left = "100%";
+	document.getElementById("second").style.display = "none";
+}
