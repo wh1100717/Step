77423e4d64272618a37384ded8230295c63efd1b
Martin0809 <maning0809@gmail.com>
2014-06-27 21:06:21 +0800

    完成scene_model的跳转 还需加入跳转效果以及页面样式

---
+++ BackManage/app/scripts/move.js
@@ -0,0 +1,5 @@
+function move() {
+	document.getElementById("first").style.left = "-100%";
+	document.getElementById("second").style.left = "0";
+	document.getElementById("second").style.display = "block";
+}
