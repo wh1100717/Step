ce8db49861f2a89f891393805f663a77e5324c8c
Martin0809 <maning0809@gmail.com>
2014-06-28 14:10:08 +0800

    完成scene_model跳转效果 note：js实现没有css3效果好，可是css3目前存在部分问题

--- BackManage/app/scripts/move.js
+++ BackManage/app/scripts/move.js
@@ -1,11 +1,29 @@
 function move() {
-	document.getElementById("first").style.left = "-100%";
-	document.getElementById("second").style.left = "0";
-	document.getElementById("second").style.display = "block";
+	var n = 0;
+    var m = 100;
+    document.getElementById("second").style.display = "block";
+    moveTimer = setInterval(function() {
+      n--;
+      m--;
+      document.getElementById("first").style.left = n + '%';
+      document.getElementById("second").style.left = m + '%';
+      if (n == -100 || m == 0) {
+        clearInterval(moveTimer);
+      } 
+    }, 1);
 }
 
 function back() {
-	document.getElementById("first").style.left = "0";
-	document.getElementById("second").style.left = "100%";
-	document.getElementById("second").style.display = "none";
+	var n = -100;
+    var m = 0;
+    backTimer = setInterval(function() {
+      n++;
+      m++;
+      document.getElementById("first").style.left = n + '%';
+      document.getElementById("second").style.left = m + '%';
+      if (n == 0 || m == 100) {
+      	document.getElementById("second").style.display = "none";
+        clearInterval(backTimer);
+      } 
+    }, 1);
 }
